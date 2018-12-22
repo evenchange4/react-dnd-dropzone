4b7c1beb43f915975e25bebfbbe70612cfa0fb26
jaxyn <jaxyn@sutu.re>
2018-12-22 19:48:05 +0900

    Add Storybook test

--- src/Dropzone.example.js
+++ src/Dropzone.example.js
@@ -2,25 +2,58 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import Dropzone from '.';
 
-storiesOf('Dropzone', module).add(
-  'Basic',
-  () => (
-    <Dropzone
-      onDrop={files => action('onDrop')(files)}
-      render={({ canDrop, isOver }) => (
-        <div style={{ border: '1px dashed black', height: 200 }}>
-          Drop file here!
-          <pre>{JSON.stringify({ canDrop, isOver }, null, 2)}</pre>
-        </div>
-      )}
-    />
-  ),
-  {
-    info: {
-      text: `API: https://github.com/evenchange4/react-dnd-dropzone#api`,
-      inline: true,
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import Dropzone, { DropzoneWithoutContext } from './Dropzone';
+
+const HTML5DropContext = DragDropContext(HTML5Backend)(({ children }) => (
+  <div>{children}</div>
+));
+
+storiesOf('Dropzone', module)
+  .add(
+    'Basic',
+    () => (
+      <Dropzone
+        onDrop={files => action('onDrop')(files)}
+        render={({ canDrop, isOver }) => (
+          <div style={{ border: '1px dashed black', height: 200 }}>
+            Drop file here!
+            <pre>{JSON.stringify({ canDrop, isOver }, null, 2)}</pre>
+          </div>
+        )}
+      />
+    ),
+    {
+      info: {
+        text: `API: https://github.com/evenchange4/react-dnd-dropzone#api`,
+        inline: true,
+      },
     },
-  },
-);
+  )
+  .add(
+    'NoContext',
+    () => (
+      <HTML5DropContext>
+        <DropzoneWithoutContext
+          onDrop={files => action('onDrop')(files)}
+          render={({ canDrop, isOver }) => (
+            <div style={{ border: '1px dashed black', height: 200 }}>
+              (Without built-in <code>HTML5Backend</code> context)
+              <br />
+              <br />
+              Drop file here!
+              <pre>{JSON.stringify({ canDrop, isOver }, null, 2)}</pre>
+            </div>
+          )}
+        />
+      </HTML5DropContext>
+    ),
+    {
+      info: {
+        text: `API: https://github.com/evenchange4/react-dnd-dropzone#api`,
+        inline: true,
+      },
+    },
+  );
